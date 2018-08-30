c620b92aa090c9bf5750d997aa255b23427bdb3f
Ian <gesner.ian@gmail.com>
2018-08-30 17:45:44 -0400

    added navigation to course project

--- UdemyCourseProject/src/app/Header/header/header.component.ts
+++ UdemyCourseProject/src/app/Header/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { SelectedView } from '../../shared/selected-view.enum';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output()
+  viewSelect =  new EventEmitter<SelectedView>();
+
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelectRecipes(){
+    console.log('recipes');
+    this.viewSelect.emit(SelectedView.Recipes);
+  }
+
+  onSelectShoppingList(){
+    this.viewSelect.emit(SelectedView.ShoppingList);
+  }
 }
---
+++ UdemyCourseProject/src/app/shared/selected-view.enum.ts
@@ -0,0 +1,1 @@
+export enum SelectedView { Recipes, ShoppingList }
